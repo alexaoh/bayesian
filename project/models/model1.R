106b4d385865f8051fa6ec5cc7f86c36bc52724f
alexaoh <alexaoh@users.noreply.github.com>
2022-05-17 18:35:21 +0200

    Changed simulation of post pred (added below). Please check! + please
    also check how the valus for model checking are simulated. I think this
    makes more sense with what they have written in theory, but the
    reference distributions look kind of strange.

--- project/models/model1.R
+++ project/models/model1.R
@@ -3,7 +3,12 @@ library(bayesplot)
 library(tidyverse)
 library(Hmisc)
 library(ggplot2)
+library(ggthemes)
 library(xtable)
+
+# Set parameter that tells Stan how many cores the machine has. 
+# For computing chains in parallel. 
+options(mc.cores = parallel::detectCores()) 
 
 set.seed(1234)
 setwd("/home/ajo/gitRepos/bayesian/project/models")
@@ -85,7 +90,9 @@ dim(posterior)
 
 
 ####### Model Checking
-# Calculate the Posterior Predictive Distribution (Det stemmer vel dette?)
+# Calculate the Posterior Predictive Distribution.
+# TROR DETTE BLIR LITT FEIL?!
+# BURDE VEL HELLER BRUKE DE SIMULERTE VERDIENE FRA POSTERIOR, IKKE MEAN-VALUENE DERES?
 p.mean <- mean(posterior$p)
 mu1.mean <- mean(posterior$mu1)
 mu2.mean <- mean(posterior$mu2)
@@ -96,12 +103,12 @@ components <- sample(1:2,prob=c(p.mean,1-p.mean),size=N,replace=TRUE)
 mus <- c(mu1.mean,mu2.mean)
 sds <- c(sigma.mean,sigma.mean) 
 samples <- rnorm(N)*sds[components]+mus[components]
-tibble(samples) %>% 
+data.frame(samples, z = as.factor(components)) %>% 
   ggplot(aes(samples)) +
   geom_density(aes(y = (..count..)/sum(..count..))) +
   ggtitle("Mix of Gaussian") + 
   ylab("Posterior Predictive Distribution") +
-  xlab("Duration [days]") +
+  xlab("Duration [days]") 
 ggsave("../626fca86090ba51a6aff419a/plots/postpred1.pdf", width = 7, height = 5)
 # Cannot open file (over). Vet ikke hvorfor det ikke funker for meg. Lagret derfor plottene 
 # direkte i mappen manuelt, men bør fikses senere tenker jeg (for å få rett width/height etc).
@@ -112,27 +119,59 @@ d <- density(samples, n = N)
 plot(d)
 # Uansett er begge to en kernel density estimator. 
 
+# NY POSTERIOR PREDICTIVE. JEG TROR DENNE BLIR MER KORREKT!
+# Sjekk det ut du også! Det er ikke denne som er lagt inn i rapporten enda!
+N <- 10000
+components <- rep(NA, N)
+probs <- sample(posterior$p, size = N, replace = T)
+unf <- runif(N)
+components[unf <= probs] <- 1
+components[unf > probs] <- 2
+mus <- rep(NA, N)
+mus[unf <= probs] <- sample(posterior$mu1, size = table(unf <= probs)[[2]], replace = T)
+mus[unf > probs] <- sample(posterior$mu2, size = table(unf > probs)[[2]], replace = T)
+sd.both <- sample(posterior$sigma, size = N, replace = T)
+sds <- c(sd.both, sd.both)
+samples <- rnorm(N)*sds[components]+mus[components]
+tibble(samples) %>% 
+  ggplot(aes(samples)) +
+  geom_density(aes(y = (..count..)/sum(..count..))) +
+  ggtitle("Mix of Gaussian") + 
+  ylab("Posterior Predictive Distribution") +
+  xlab("Duration [days]")
+
 ### Model Checking. 
-n <- 10000
+n <- dim(posterior)[[1]]
 # We select the statistics 1st quart, median and 3rd quart. 
-statistic.distrs <- list(first = rep(NA, n), median = rep(NA, n), third = rep(NA, n))
+statistic.distrs <- list(first = rep(NA, n), median = rep(NA, n), 
+                         mean = rep(NA, n), third = rep(NA, n))
+
 
 for(i in 1:n){
-  # Simulate the posterior distribution. 
+  # Simulate the posterior distribution using every simulated value from MCMC fit (Stan) once. 
+  p.mean <- posterior$p[i]
+  mu1.mean <- posterior$mu1[i]
+  mu2.mean <- posterior$mu2[i]
+  sigma.mean <- posterior$sigma[i]
+  
   components <- sample(1:2,prob=c(p.mean,1-p.mean),size=N,replace=TRUE)
   mus <- c(mu1.mean,mu2.mean)
   sds <- c(sigma.mean,sigma.mean) 
   samples <- rnorm(N)*sds[components]+mus[components]
-  
+
   # Simulate from the posterior distribution.
   q <- quantile(samples, c(0.25, 0.5, 0.75))
   statistic.distrs$first[i] <- q[1]
   statistic.distrs$median[i] <- q[2]
+  statistic.distrs$mean[i] <- mean(samples)
   statistic.distrs$third[i] <- q[3]
 }
 
-# Compare with the same statistics in the data. 
-q.data <- quantile(dur, c(0.25, 0.5, 0.75)) # Replace with "train" (use the sample we trained model on).
+# Compare with the same statistics in the data. The "data" in this case is the sampled data point
+# used to fit the model fith Stan.
+q.data <- quantile(train, c(0.25, 0.5, 0.75)) 
+stat.data <- list(first = q.data[1], median = q.data[2], 
+                  mean = mean(train), third = q.data[3])
 
 # Plot shows that the first quartile in the data is highly unlikely in the reference distribution. 
 # Perhaps not a good model then!
@@ -140,18 +179,27 @@ tibble(statistic.distrs$first) %>%
   ggplot(aes(statistic.distrs$first)) +
   geom_density(aes(y = (..count..)/sum(..count..))) +
   ggtitle("First Quartile") +
-  geom_vline(xintercept = q.data[1])
+  geom_vline(xintercept = stat.data$first)
 # Also did numerical calculation.
-min(mean(statistic.distrs$first < q.data[1]), mean(statistic.distrs$first > q.data[1]))
+min(mean(statistic.distrs$first < stat.data$first), mean(statistic.distrs$first > stat.data$first))
 
 # Plot shows that median is highly unlikely.
 tibble(statistic.distrs$median) %>% 
   ggplot(aes(statistic.distrs$median)) +
   geom_density(aes(y = (..count..)/sum(..count..))) +
   ggtitle("Median") +
-  geom_vline(xintercept = q.data[2])
+  geom_vline(xintercept = stat.data$median)
 # Also did numerical calculation.
-min(mean(statistic.distrs$median < q.data[2]), mean(statistic.distrs$median > q.data[2]))
+min(mean(statistic.distrs$median < stat.data$median), mean(statistic.distrs$median > stat.data$median))
+
+# Plot shows that mean is highly unlikely.
+tibble(statistic.distrs$mean) %>% 
+  ggplot(aes(statistic.distrs$mean)) +
+  geom_density(aes(y = (..count..)/sum(..count..))) +
+  ggtitle("Mean") +
+  geom_vline(xintercept = stat.data$mean)
+# Also did numerical calculation.
+min(mean(statistic.distrs$median < stat.data$mean), mean(statistic.distrs$median > stat.data$mean))
 
 # Plot shows that third quartile is highly unlikely.
 # Model is not good according to any of these three statistics. 
@@ -159,6 +207,6 @@ tibble(statistic.distrs$third) %>%
   ggplot(aes(statistic.distrs$third)) +
   geom_density(aes(y = (..count..)/sum(..count..))) +
   ggtitle("Third Quartile") +
-  geom_vline(xintercept = q.data[3])
+  geom_vline(xintercept = stat.data$third)
 # Also did numerical calculation.
-min(mean(statistic.distrs$third < q.data[3]), mean(statistic.distrs$third > q.data[3]))
+min(mean(statistic.distrs$third < stat.data$third), mean(statistic.distrs$third > stat.data$third))
