1e82907769dd3441df72085cec0956845fc3d995
alexaoh <alexaoh@users.noreply.github.com>
2022-05-17 19:22:37 +0200

    Added file "sample_data_points.R" --> Samples 10k points from data and
    saves it in a rds file, which then can be loadedd into R files later.

--- project/models/model1.R
+++ project/models/model1.R
@@ -13,21 +13,22 @@ options(mc.cores = parallel::detectCores())
 set.seed(1234)
 setwd("/home/ajo/gitRepos/bayesian/project/models")
 # Read the data that was cleaned in "erasmus.R".
-data <- read.csv("../cleaned.csv")
-summary(data)
-dim(data)
-
-# Change the data types again. 
-data <- data %>% transmute(duration = as.numeric(duration),
-                           age = as.numeric(age),
-                           gender = as.factor(gender),
-                           nationality = as.factor(nationality),
-                           sending.country = as.factor(sending.country),
-                           receiving.country = as.factor(receiving.country),
-                           activity = as.factor(activity),
-                           participants = participants)
-
-describe(data)
+# data <- read.csv("../cleaned.csv")
+# summary(data)
+# dim(data)
+# 
+# # Change the data types again. 
+# data <- data %>% transmute(duration = as.numeric(duration),
+#                            age = as.numeric(age),
+#                            gender = as.factor(gender),
+#                            nationality = as.factor(nationality),
+#                            sending.country = as.factor(sending.country),
+#                            receiving.country = as.factor(receiving.country),
+#                            activity = as.factor(activity),
+#                            participants = participants)
+# 
+# describe(data)
+data <- readRDS("../10kpoints.rds") # Load the sampled data. 
 
 # We want to model the duration of the student exchange. 
 dur <- data$duration 
@@ -36,25 +37,24 @@ any(is.na(dur)) # Checking to be sure that there are no NA here.
 # We simulate values from the posterior distribution using Stan. 
 # Define model and call stan. 
 
-points <- 10000
-train <- sample(dur, size = points)
+points <- dim(data)[[1]] # Or length(dur)
 data_list <- list(
   n=points,
-  y=train # Sample `points` number of points from the dataset.
+  y=dur # Sample `points` number of points from the dataset.
 )
 
-# fit1 <- stan("../stan_models/model1.stan", iter = 1000, chains = 4,
-#             data = data_list, seed = 1)
+fit1 <- stan("../stan_models/model1.stan", iter = 1000, chains = 4,
+             data = data_list, seed = 1)
 
 # Save the fitted object in order to not run again every time. 
 # Analysis can easily be done later by loading this object. 
-save(fit1, train, file="model1_10k.RData") # Used for saving several objects. 
-#saveRDS(fit1, file = "model1FIT50k.rds") # Used for saving one object. 
+#save(fit1, train, file="model1_10k.RData") # Used for saving several objects. 
+saveRDS(fit1, file = "model1_FIT10k.rds") # Used for saving one object. 
 
-#fit1 <- readRDS("model1FIT50k.rds") # Load one object.
+fit1 <- readRDS("model1FIT50k.rds") # Load one object.
 # Load several objects into scope.
 # In this case we load "fit1" and "train".
-load(file = "model1_10k.RData")
+#load(file = "model1_10k.RData")
 
 # Convergence analysis.
 print(fit1)
