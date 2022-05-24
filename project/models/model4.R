ab9abe7c29114e60449dfc52728d9890af9d925b
ulrikbd <ulrik_danielsen@hotmail.com>
2022-05-24 20:22:31 +0200

    Created the 15k model4 df. Now model4.stan uses invgamma(10,1000). Added some posterior plots for beta1 beta2.

--- project/models/model4.R
+++ project/models/model4.R
@@ -10,29 +10,31 @@ setwd("/home/ajo/gitRepos/bayesian/project/models")
 
 # Read the training data set that was sampled.
 data <- readRDS("../15kpoints.rds") # Load the sampled data. 
+
+# Omit the unknown genders for our regression model
+data <- data %>% filter(gender == "Female" | gender == "Male")
+data$gender <- as.integer(data$gender) - 1
 describe(data)
 
 # Sample 5k points from the 15k sample to begin with. 
-points <- 5000
-sample_df <- data[sample(1:nrow(data), points),]
-summary(sample_df)
+points <- dim(data)[[1]] 
 
 # We simulate values from the posterior distribution using Stan. 
 # Define model and call stan. 
 
 data_list <- list(
   n=points,
-  y=sample_df$duration,
-  x1=sample_df$age,
-  x2=sample_df$gender
+  y=data$duration,
+  x1=data$age,
+  x2=as.numeric(data$gender)
 )
 
 fit4 <- stan("../stan_models/model4.stan", iter = 1000, chains = 4,
              data = data_list, seed = 1)
-
 # Save the fitted object in order to not run again every time. 
 # Analysis can easily be done later by loading this object. 
 saveRDS(fit4, file = "../model4_FIT15k.rds") # Used for saving one object. 
+
 
 # Load the (already) generated object into scope. 
 fit4 <- readRDS("../model4_FIT15k.rds") # Load one object.
@@ -54,6 +56,21 @@ tibble(y_pred) %>%
   ggtitle("Mix of Gaussian")
 ggsave("../626fca86090ba51a6aff419a/plots/postpred4.pdf", width = 7, height = 5)
 
+beta1 <- posterior[, "beta1"]
+tibble(beta1) %>% 
+  ggplot(aes(beta1)) +
+  geom_density(aes(y = (..count..)/sum(..count..))) +
+  ggtitle("Posterior distribution of beta1")
+ggsave("../626fca86090ba51a6aff419a/plots/postbeta1.pdf", width = 7, height = 5)
+
+beta2 <- posterior[, "beta2"]
+tibble(beta2) %>% 
+  ggplot(aes(beta2)) +
+  geom_density(aes(y = (..count..)/sum(..count..))) +
+  ggtitle("Posterior distribution of beta2")
+ggsave("../626fca86090ba51a6aff419a/plots/postbeta2.pdf", width = 7, height = 5)
+
+
 
 # We select the statistics 1st quart, median and 3rd quart. 
 q.sim <- quantile(y_pred, c(0.25, 0.5, 0.75))
@@ -67,32 +84,5 @@ quantile(posterior$beta1, c(0.025, 0.975))
 quantile(posterior$beta2, c(0.025, 0.975))
 
 mean(posterior$beta1)
+mean(posterior$beta2)
 
-# Plot shows that the first quartile in the data is highly unlikely in the reference distribution. 
-# Perhaps not a good model then!
-tibble(statistic.distrs$first) %>% 
-  ggplot(aes(statistic.distrs$first)) +
-  geom_density(aes(y = (..count..)/sum(..count..))) +
-  ggtitle("First Quartile") +
-  geom_vline(xintercept = q.data[1])
-# Also did numerical calculation.
-min(mean(statistic.distrs$first < q.data[1]), mean(statistic.distrs$first > q.data[1]))
-
-# Plot shows that median is highly unlikely.
-tibble(statistic.distrs$median) %>% 
-  ggplot(aes(statistic.distrs$median)) +
-  geom_density(aes(y = (..count..)/sum(..count..))) +
-  ggtitle("Median") +
-  geom_vline(xintercept = q.data[2])
-# Also did numerical calculation.
-min(mean(statistic.distrs$median < q.data[2]), mean(statistic.distrs$median > q.data[2]))
-
-# Plot shows that third quartile is highly unlikely.
-# Model is not good according to any of these three statistics. 
-tibble(statistic.distrs$third) %>% 
-  ggplot(aes(statistic.distrs$third)) +
-  geom_density(aes(y = (..count..)/sum(..count..))) +
-  ggtitle("Third Quartile") +
-  geom_vline(xintercept = q.data[3])
-# Also did numerical calculation.
-min(mean(statistic.distrs$third < q.data[3]), mean(statistic.distrs$third > q.data[3]))
