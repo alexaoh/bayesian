c3b31c6f679dbab3a1c8ddd8bdcf1452e8d9dfc4
ulrikbd <ulrik_danielsen@hotmail.com>
2022-05-25 12:57:45 +0200

    Worked on the prior predictive distribution choices, looks like invgamma(10,1000) works

--- project/prior_predictive.R
+++ project/prior_predictive.R
@@ -4,28 +4,34 @@ library(invgamma)
 library(tidyverse)
 library(ggplot2)
 
-delta.x <- 0.001
+delta.x <- 0.1
 x <- seq(0, 500, delta.x)
 N <- length(x)
 m1 <- 120
 m2 <- 280
-sigma.sq <- rinvgamma(N, 10, 10000)
+sigma.sq <- rinvgamma(N, 10, 1000)
 sigma <- rinvgamma(N, 1, 1)
 
-s <- seq(0, 5, .01)
-plot(s, dinvgamma(s, 10, 10000), type = 'l')
+s <- seq(0, 500, .01)
+plot(s, dinvgamma(s, 10, 1000), type = 'l')
 
 hist(sigma.sq, breaks = 50, freq = F)
 
-prior.mu1 <- dnorm(x, mean = m1, sd = sigma)
+prior.mu1 <- rnorm(x, mean = m1, sd = sigma.sq)
 plot(density(prior.mu1))
-prior.mu2 <- dnorm(x, mean = m2, sd = sigma)
+prior.mu2 <- rnorm(x, mean = m2, sd = sigma.sq)
+plot(density(prior.mu2))
 tibble(x, prior.mu1, prior.mu2) %>% 
   ggplot() +
-  geom_line(aes(x = x, y = prior.mu1, linetype = "c")) +
-  geom_line(aes(x = x, y = prior.mu2, linetype = "e")) +
+  geom_density(aes(x = prior.mu1, linetype = "c")) +
+  geom_density(aes(x = prior.mu2, linetype = "e")) +
   ggtitle("Prior Distributions") +
   ylab("Density") +
   scale_linetype_manual(name = "Mean Parameters", 
                        values = 1:2, labels = c("mu1", "mu2"))
-#+ ggsave("../626fca86090ba51a6aff419a/plots/priorpreds.pdf", width = 7, height = 5)
+ggsave("../626fca86090ba51a6aff419a/plots/priorpreds.pdf", width = 7, height = 5)
+
+prior.p <- runif(x)
+pror.y_pred <- prior.p*rnorm(x, mean = prior.mu1, sigma.sq) +
+                (1 - prior.p)*rnorm(x, mean = prior.mu2, sigma.sq)
+plot(density(prior.mu2))
